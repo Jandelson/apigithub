71192cd33e3389916dd2e9eb5a95ccde2acccc94
Jandelson <jandelson.oliveira@sapientag2.com.br>
2021-11-26 17:36:25 -0300

    Electron build windows

---
+++ app/main.js
@@ -0,0 +1,32 @@
+const setupEvents = require('../installers/setupEvents')
+ if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+ }
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 768,
+  })
+
+  win.loadFile(path.join(__dirname, '../app/index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
